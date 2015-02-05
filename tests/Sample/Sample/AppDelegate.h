098bee9b684b37f1ec92ea0ac89db75fb35da583
Jason Jarrett <jason@elegantcode.com>
2015-02-05 12:47:43 -0800

    basic sample project

---
+++ tests/Sample/Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sample
+//
+//  Created by Jason Jarrett on 2/5/15.
+//  Copyright (c) 2015 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tests/Sample/Sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sample
+//
+//  Created by Jason Jarrett on 2/5/15.
+//  Copyright (c) 2015 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
